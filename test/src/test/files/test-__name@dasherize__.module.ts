6e674d202742979096e2b991304962f42b218b7c
Eleonora Zbarskaya <39634549+kingofferelden@users.noreply.github.com>
2019-08-23 21:43:47 +0300

    Update test-__name@dasherize__.module.ts

--- test/src/test/files/test-__name@dasherize__.module.ts
+++ test/src/test/files/test-__name@dasherize__.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { DashboardCommonModule } from '@tinkoff/afd-ui/view/dashboard/common/dashboard-common.module';
+import { DashboardCommonModule } from 'common/dashboard-common.module';
 import { <%= classify(name) %>Component } from './<%= dasherize(name) %>.component';
 import { <%= classify(name) %>RoutingModule } from './<%= dasherize(name) %>-routing.module';
 import { Api<%= classify(name) %>ClientService } from './services/<%= dasherize(name) %>.service';
